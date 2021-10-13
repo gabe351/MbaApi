dbed62c189bb17c2ec4492c83564a41d139b7b33
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-10-13 18:38:19 -0300

    Feature 1 - Created User Table

---
+++ src/main/java/com/gabea/mbaapi/model/enums/InstrumentEnum.java
@@ -0,0 +1,13 @@
+package com.gabea.mbaapi.model.enums;
+
+public enum InstrumentEnum {
+
+    AGBE,
+    AGOGO,
+    GONGUE,
+    CAIXA,
+    ALFAIA,
+    APITO,
+    CANTO;
+
+}
---
+++ src/main/java/com/gabea/mbaapi/model/enums/UserTypeEnum.java
@@ -0,0 +1,7 @@
+package com.gabea.mbaapi.model.enums;
+
+public enum UserTypeEnum {
+    ADMIN,
+    DEV,
+    REGULAR;
+}
